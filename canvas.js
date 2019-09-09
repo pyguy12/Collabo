15fa195dbf3914059ff8ed9249389f86ae17a4ab
pyguy12 <wuwarren7360@gmail.com>
2019-09-09 09:07:47 -0500

    Added files and began working on brush feature

--- canvas.js
+++ canvas.js
@@ -2,5 +2,43 @@ let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 let canvasContainer = document.getElementById('canvas-container');
 
+//Sets canvas height and width to the height and width of the div containing the canvas element.
 canvas.height = canvasContainer.offsetHeight;
 canvas.width = canvasContainer.offsetWidth;
+
+let mouse = {
+  x: null,
+  y: null
+};
+
+ctx.lineWidth = 10;
+
+canvas.addEventListener('mousemove', e => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
+
+canvas.addEventListener('mousedown', () => {
+  ctx.beginPath();
+  ctx.moveTo(mouse.x, mouse.y);
+
+  canvas.addEventListener('mousemove', draw);
+});
+
+canvas.addEventListener('mouseup', () => {
+  canvas.removeEventListener('mousemove', draw);
+});
+
+const draw = () => {
+  ctx.lineTo(mouse.x, mouse.y);
+  ctx.stroke();
+};
+
+//Click handlers for buttons
+const handleBrushClick = () => {
+  console.log('brush clicked');
+};
+
+const handleEraserClick = () => {
+  console.log('eraser clicked');
+};
